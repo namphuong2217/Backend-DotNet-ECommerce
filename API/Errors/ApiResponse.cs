e9ed34a7f44c7943eea3902c6a3a73b1991db495
namphuong2217 <namphuong2217@gmail.com>
2021-03-21 16:49:43 +0100

    Complete Error Handling

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse()
+        {
+        }
+
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch 
+            {
+                400 => "A bad request ",
+                401 => "Unauthorized,",
+                404 => "Resource not found",
+                500 => "Errors",
+                _ => null
+            };
+        }
+    }
+}
