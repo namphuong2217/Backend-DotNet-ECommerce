98164da499faf1ba18e0060c21176f241833423e
namphuong2217 <namphuong2217@gmail.com>
2021-04-03 09:42:16 +0200

    Router

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -26,7 +27,7 @@ export class ShopService {
       params = params.append('typeId', shopParams.typeId.toString());
     }
 
-    if(shopParams.search){
+    if (shopParams.search) {
       params = params.append('search', shopParams.search);
     }
 
@@ -44,6 +45,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
