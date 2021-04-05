6541e6eb123f3b4647fcf4741cfa15766b32c0f2
namphuong2217 <namphuong2217@gmail.com>
2021-04-05 19:45:39 +0200

    .NET Validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -71,14 +71,15 @@ namespace API.Controllers
 
         [Authorize]
         [HttpPut("address")]
-        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address){
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+        {
             var user = await _userManager.FindUserByClaimsPrinciplelWithAddressAsync(HttpContext.User);
 
             user.Address = _mapper.Map<AddressDto, Address>(address);
 
             var result = await _userManager.UpdateAsync(user);
 
-            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem at updating the user");
         }
@@ -106,6 +107,10 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" } });
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        // [RegularExpression("^[a-zA-Z]\\w{3,14}$", ErrorMessage = "The password's first character must be a letter, it must contain at least 4 characters and no more than 15 characters and no characters other than letters, numbers and the underscore may be used")]
         public string Password { get; set; }
     }
 }
